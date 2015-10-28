143520b2fceb18316069f35d644ede43b967726e
Jan Muehlemann <jan.muehlemann@gmail.com>
2015-10-28 14:23:02 +0100

    update publish

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp'),
+    prompt = require('gulp-prompt'),
     git = require('gulp-git'),
     bump = require('gulp-bump'),
     filter = require('gulp-filter'),
     tag_version = require('gulp-tag-version'),
+    shell = require('gulp-shell'),
     argv = require('yargs').argv,
     gulpif = require('gulp-if'),
     uglify = require('gulp-uglify'),
@@ -15,11 +17,14 @@ var gulp = require('gulp'),
     eslint = require('gulp-eslint'),
     Server = require('karma').Server;
 
+var pkg = require('./package.json');
+
 var entry = 'index.js',
+    standaloneName = 'i18nextBrowserLanguageDetector',
     output = 'index.js';
 
 function compile(watch) {
-  var bundler = browserify('./lib/' + entry, { debug: argv.debug, standalone: 'i18nextBrowserLanguageDetector' }).transform(babel);
+  var bundler = browserify('./lib/' + entry, { debug: argv.debug, standalone: standaloneName }).transform(babel);
   if (watch) {
     bundle = watchify(bundler);
   }
@@ -30,8 +35,8 @@ function compile(watch) {
       .pipe(source(output))
       .pipe(buffer())
       .pipe(gulpif(!argv.debug, uglify()))
-      .pipe(sourcemaps.init({ loadMaps: true }))
-      .pipe(sourcemaps.write('./'))
+      .pipe(gulpif(argv.debug, sourcemaps.init({ loadMaps: true })))
+      .pipe(gulpif(argv.debug, sourcemaps.write('./')))
       .pipe(gulp.dest('./bin'));
   }
 
@@ -78,6 +83,26 @@ gulp.task('tdd', function (done) {
 function inc(version) {
   if (!version) return;
 
+  var type;
+
+  if (version.indexOf('.') < 0) {
+    if (version === 'major' || version === 'minor' || version === 'patch') {
+      type = version;
+    } else {
+      var tag = version;
+
+      var parts = pkg.version.split('-');
+      if (parts.length > 1) {
+        var tagVersion = 0;
+        var p = parts[1].split('.');
+        if (p[0] === tag) tagVersion = parseInt(p[1], 10) + 1;
+        version = parts[0] + '-' + tag  + '.' + tagVersion;
+      } else {
+        version = pkg.version + '-' + tag  + '.0';
+      }
+    }
+  }
+
   function cb(obj) {
     var stream = new require('stream').Transform({objectMode: true});
     stream._transform = function(file, unused, callback) {
@@ -90,7 +115,7 @@ function inc(version) {
   // get all the files to bump version in
   return gulp.src(['./package.json', './bower.json'])
     // bump the version number in those files
-    .pipe(bump({version: version}))
+    .pipe(type ? bump({type: type}) : bump({version: version}))
     // save it back to filesystem
     .pipe(gulp.dest('./'))
     // commit the changed version number
@@ -102,11 +127,24 @@ function inc(version) {
     .pipe(tag_version({prefix: ''}))
 
     // push tag
+    .pipe(prompt.confirm({
+        message: 'Push tag ' + version + ' to github?',
+        default: false
+    }))
     .pipe(cb(function() {
       git.push('origin','master', {args: ' --tags'}, function (err) {
         if (err) throw err;
       });
-    }));
+    }))
+
+    // npm publish
+    .pipe(prompt.confirm({
+        message: 'publish ' + version + ' to npm?',
+        default: false
+    }))
+    .pipe(shell([
+      'npm publish'
+    ]));
 }
 
 function watch() {
