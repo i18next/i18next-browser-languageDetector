53de5b5381c2620fc0176b76f75e10687d9adfa4
Jan Muehlemann <jan.muehlemann@gmail.com>
2015-10-29 09:19:37 +0100

    change publish

--- gulpfile.js
+++ gulpfile.js
@@ -126,6 +126,9 @@ function inc(version) {
     // save it back to filesystem
     .pipe(gulp.dest('./'))
 
+    // commit change
+    .pipe(git.commit('bumps package version'))
+
     // read only one file to get the version number
     .pipe(filter('package.json'))
     // **tag it in the repository**
@@ -133,10 +136,9 @@ function inc(version) {
 
     // push tag
     .pipe(prompt.confirm({
-        message: 'Commit and Push tag ' + version + ' to github?',
+        message: 'Push tag ' + version + ' to github?',
         default: false
     }))
-    .pipe(git.commit('bumps package version'))
     .pipe(cb(function() {
       git.push('origin','master', {args: ' --tags'}, function (err) {
         if (err) throw err;
