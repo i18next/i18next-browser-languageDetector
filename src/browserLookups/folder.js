864eef10272e1b35ecad86aa08d67ed30c7bec30
Johannes Terhürne <johannes@terhuerne.org>
2018-04-05 10:04:44 +0200

    Check if language const contains array

--- src/browserLookups/folder.js
+++ src/browserLookups/folder.js
@@ -5,10 +5,12 @@ export default {
     let found;
     if (typeof window !== 'undefined') {
       const language = window.location.pathname.match(/\/([a-zA-Z-]*)/g);
-      if (typeof options.lookupFromUrlIndex === 'number') {
-        found = language[options.lookupFromFolderIndex].replace('/', '');
-      } else {
-        found = language[0].replace('/', '');
+      if (language instanceof Array) {
+        if (typeof options.lookupFromUrlIndex === 'number') {
+          found = language[options.lookupFromFolderIndex].replace('/', '');
+        } else {
+          found = language[0].replace('/', '');
+        }
       }
     }
     return found;
