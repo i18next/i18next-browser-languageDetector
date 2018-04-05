84b84eb2ad77896e4e343b00b8275477c1b34c7d
Johannes Terhürne <johannes@terhuerne.org>
2018-04-05 10:05:16 +0200

    Run build again

--- i18nextBrowserLanguageDetector.js
+++ i18nextBrowserLanguageDetector.js
@@ -169,10 +169,12 @@
       var found = void 0;
       if (typeof window !== 'undefined') {
         var language = window.location.pathname.match(/\/([a-zA-Z-]*)/g);
-        if (typeof options.lookupFromUrlIndex === 'number') {
-          found = language[options.lookupFromFolderIndex].replace('/', '');
-        } else {
-          found = language[0].replace('/', '');
+        if (language instanceof Array) {
+          if (typeof options.lookupFromUrlIndex === 'number') {
+            found = language[options.lookupFromFolderIndex].replace('/', '');
+          } else {
+            found = language[0].replace('/', '');
+          }
         }
       }
       return found;
