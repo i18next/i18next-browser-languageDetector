02ccf08fbc1e501e628f40948e5b0a642f7461bd
Kevin Ross <kevin.ross@alienfast.com>
2019-08-07 12:47:07 -0500

    tsc test interop vs non-interop

---
+++ test/typescript/usageInterop.ts
@@ -0,0 +1,60 @@
+import LngDetector, { CustomDetector, DetectorOptions } from 'i18next-browser-languagedetector';
+import i18next from 'i18next';
+
+/**
+ * NOTE: only the imports should differ in these `usage*.ts` files
+ */
+
+const options: DetectorOptions = {
+  // order and from where user language should be detected
+  order: ['querystring', 'cookie', 'localStorage', 'navigator', 'htmlTag'],
+
+  // keys or params to lookup language from
+  lookupQuerystring: 'lng',
+  lookupCookie: 'i18next',
+  lookupLocalStorage: 'i18nextLng',
+
+  // cache user language on
+  caches: ['localStorage', 'cookie'],
+  excludeCacheFor: ['cimode'], // languages to not persist (cookie, localStorage)
+
+  // optional expire and domain for set cookie
+  cookieMinutes: 10,
+  cookieDomain: 'myDomain',
+
+  // optional htmlTag with lang attribute, the default is:
+  htmlTag: document.documentElement,
+};
+
+const customDetector: CustomDetector = {
+  name: 'myDetectorsName',
+
+  lookup(options: DetectorOptions) {
+    // options -> are passed in options
+    return 'en';
+  },
+
+  cacheUserLanguage(lng: string, options: DetectorOptions) {
+    // options -> are passed in options
+    // lng -> current language, will be called after init and on changeLanguage
+    // store it
+  },
+};
+
+const customDetector2: CustomDetector = {
+  name: 'myDetectorsName',
+  lookup(options: DetectorOptions) {
+    return undefined;
+  },
+};
+
+const lngDetector = new LngDetector(null, options);
+
+lngDetector.init(options);
+lngDetector.addDetector(customDetector);
+
+// instance based
+i18next.use(lngDetector).init({});
+
+// class based
+i18next.use(LngDetector).init({});
