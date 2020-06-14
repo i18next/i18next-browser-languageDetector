73df96cd63b376d9512cddce9b32eae730174151
udf2457 <udf2457@users.noreply.github.com>
2020-06-14 18:01:01 +0100

    Update i18nextBrowserLanguageDetector.js
    sessionStorage

--- i18nextBrowserLanguageDetector.js
+++ i18nextBrowserLanguageDetector.js
@@ -156,6 +156,37 @@
     }
   };
 
+let hasSessionStorageSupport;
+try {
+  hasSessionStorageSupport = window !== 'undefined' && window.sessionStorage !== null;
+  const testKey = 'i18next.translate.boo';
+  window.sessionStorage.setItem(testKey, 'foo');
+  window.sessionStorage.removeItem(testKey);
+} catch (e) {
+  hasSessionStorageSupport = false;
+}
+
+export default {
+  name: 'sessionStorage',
+
+  lookup(options) {
+    let found;
+
+    if (options.lookupsessionStorage && hasSessionStorageSupport) {
+      const lng = window.sessionStorage.getItem(options.lookupsessionStorage);
+      if (lng) found = lng;
+    }
+
+    return found;
+  },
+
+  cacheUserLanguage(lng, options) {
+    if (options.lookupsessionStorage && hasSessionStorageSupport) {
+      window.sessionStorage.setItem(options.lookupsessionStorage, lng);
+    }
+  }
+};
+
   var navigator$1 = {
     name: 'navigator',
     lookup: function lookup(options) {
@@ -244,7 +275,7 @@
 
   function getDefaults() {
     return {
-      order: ['querystring', 'cookie', 'localStorage', 'navigator', 'htmlTag'],
+      order: ['querystring', 'cookie', 'sessionStorage',localStorage', 'navigator', 'htmlTag'],
       lookupQuerystring: 'lng',
       lookupCookie: 'i18next',
       lookupLocalStorage: 'i18nextLng',
@@ -286,6 +317,7 @@
         this.addDetector(cookie$1);
         this.addDetector(querystring);
         this.addDetector(localStorage);
+	this.addDetector(sessionStorage);
         this.addDetector(navigator$1);
         this.addDetector(htmlTag);
         this.addDetector(path);
